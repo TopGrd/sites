9105d18d0f0b48c3c8044fc2ce344f65cf4f57bc
topgrd <topgrd@outlook.com>
2019-01-09 00:11:34 +0800

    feat: webpack config init

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,31 @@
-// https://segmentfault.com/a/1190000013531894
-// https://github.com/crsandeep/simple-react-full-stack
-const Koa = require('koa')
-const koaWebpack = require('koa-webpack')
-const webpack = require('webpack')
-const config = require('../webpack/dev.js')
+// https://github.com/shellscape/koa-webpack
+const Koa = require('koa');
+const koaWebpack = require('koa-webpack');
+const webpack = require('webpack');
+const serve = require('koa-static');
+const config = require('../../webpack/dev.js');
 
-const compiler = webpack(config)
+const compiler = webpack(config);
 
-const app = new Koa()
+const start = async () => {
+  const app = new Koa();
+  app.use(serve('./dist'));
 
-app.use(koaWebpack({ compiler }))
+  const middleware = await koaWebpack({
+    compiler,
+    // https://github.com/webpack/webpack-dev-middleware
+    devMiddleware: {
+      logLevel: 'error',
+    },
+    // https://github.com/webpack-contrib/webpack-hot-client
+    hotClient: {
+      logLevel: 'error',
+    },
+  });
 
-app.listen(3000)
+  app.use(middleware);
+  app.listen(3000);
+  console.log('server listening on port 3000');
+};
 
-console.log('lostening on port 3000')
+start();
